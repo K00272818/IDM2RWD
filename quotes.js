bb7345d4dbe7ca406524149d9d11f35341dd1f37
K00272818 <114760850+K00272818@users.noreply.github.com>
2022-10-19 10:52:46 +0100

    Add files via upload

--- quotes.js
+++ quotes.js
@@ -1,82 +1,82 @@
 const quotes=[
     {
-        "text": "You miss 100% of the shots you don't take",
-        "author": "Wayne Gretzky"
+        text: "You miss 100% of the shots you don't take",
+        author: "Wayne Gretzky"
     },
     {
-        "text": "Everything you've ever wanted is sitting on the other side of fear",
-        "author": "George Addair"
+        text: "Everything you've ever wanted is sitting on the other side of fear",
+        author: "George Addair"
     },
     {
-        "text": "Start where you are. Use what you have. Do what you can",
-        "author": "Arthur Ashe"
+        text: "Start where you are. Use what you have. Do what you can",
+        author: "Arthur Ashe"
     },
     {
-        "text": "Make sure your worse enemy doesn't live between your own two ears",
-        "author": "Laird Hamilton"
+        text: "Make sure your worse enemy doesn't live between your own two ears",
+        author: "Laird Hamilton"
     },
     {
-        "text": "You can never leave footprints that last if you are always walking on tiptoe",
-        "author": "Leymah Gbowee"
+        text: "You can never leave footprints that last if you are always walking on tiptoe",
+        author: "Leymah Gbowee"
     },
     {
-        "text": "You can be the ripest, juiciest peach in the world and there's still going to be somebody who hates peaches",
-        "author": "Dita Von Teese"
+        text: "You can be the ripest, juiciest peach in the world and there's still going to be somebody who hates peaches",
+        author: "Dita Von Teese"
     },
     {
-        "text": "If there is no struggle, there is no progress",
-        "author": "Frederick Douglass"
+        text: "If there is no struggle, there is no progress",
+        author: "Frederick Douglass"
     },
     {
-        "text": "You carry the passport to your own happiness",
-        "author": "Diane von Furstenberg"
+        text: "You carry the passport to your own happiness",
+        author: "Diane von Furstenberg"
     },
     {
-        "text": "A year from now you will wish you had started today",
-        "author": null
+        text: "A year from now you will wish you had started today",
+        author: null
     },
     {
-        "text": "The greater the difficulty, the more glory in surmounting it",
-        "author": "Epicurus"
+        text: "The greater the difficulty, the more glory in surmounting it",
+        author: "Epicurus"
     },
     {
-        "text": "Take your victories, whatever they may be, cherish them, use them, but don't settle for them",
-        "author": "Mia Hamm"
+        text: "Take your victories, whatever they may be, cherish them, use them, but don't settle for them",
+        author: "Mia Hamm"
     },
     {
-        "text": "Someone's sitting in the shade today because someone planted a tree a long time ago",
-        "author": "Warren Buffet"
+        text: "Someone's sitting in the shade today because someone planted a tree a long time ago",
+        author: "Warren Buffet"
     },
     {
-        "text": "Opportunities don't happen, you create them",
-        "author": "Chris Grosser"
+        text: "Opportunities don't happen, you create them",
+        author: "Chris Grosser"
     },
     {
-        "text": "Work until your bank account looks like a phone number",
-        "author": null
+        text: "Work until your bank account looks like a phone number",
+        author: null
     },
     {
-        "text": "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest",
-        "author": "Walter Anderson"
+        text: "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest",
+        author: "Walter Anderson"
     },
     {
-        "text": "Setting goals is the first step in turning the invisible into the visible",
-        "author": "Tony Robbins"
+        text: "Setting goals is the first step in turning the invisible into the visible",
+        author: "Tony Robbins"
     },
     {
-        "text": "Set your goals high and don't stop till you get there",
-        "author": "Bo Jackson"
+        text: "Set your goals high and don't stop till you get there",
+        author: "Bo Jackson"
     },
     {
-        "text": "You can't cross the sea merely by standing and staring at the water",
-        "author": "Rabindranath Tagore"
+        text: "You can't cross the sea merely by standing and staring at the water",
+        author: "Rabindranath Tagore"
     },
     {
-        "text": "Dont't think just do",
-        "author": "Horace"
+        text: "Dont't think just do",
+        author: "Horace"
     },
     {
-        "text": "A good plan violently executed now is better than a perfect plan existed next week",
-        "author": "George S. Patton"
+        text: "A good plan violently executed now is better than a perfect plan existed next week",
+        author: "George S. Patton"
     }
 ]
